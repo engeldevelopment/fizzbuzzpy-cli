5f7b182f86b8403be5385e4219ff95a66068cf93
engel <engeljavierpinto@gmail.com>
2021-04-05 11:38:15 -0400

    docs: Documenté un poco el comando fizzbuzz.

--- setup.py
+++ setup.py
@@ -5,7 +5,9 @@ setup(
     name="fizzbuzz",
     version="1.0.0",
     author="Engel Pinto",
-    install_requires=["Click"],
+    description="FizzBuzz Kata.",
+    author_email="engeljavierpinto@gmail.com",
+    install_requires=["Click==7.1.2"],
     packages=find_namespace_packages(include="fizzbuzz"),
     include_package_data=True,
     entry_points="""
